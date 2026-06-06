903814923d067c7f4dea5698fe39e1fa5d72893e
fennylol <jfenneran@cedarville.edu>
2026-06-06 18:35:54 -0400

    works

--- main.gd
+++ main.gd
@@ -4,43 +4,44 @@ extends Node
 var my_own_little_pingus: PingusPrime = PingusPrime.new()
 
 func _ready() -> void:
-	menu.connect_button_pressed.connect(touch_something_with_my_own_little_pingus)
-	menu.send_msg_button_pressed.connect(touch_something_with_my_own_little_stringus)
-	my_own_little_pingus.message_recieved.connect(_recieve_message)
-	my_own_little_pingus.set_name("my_own_little_pingus")
-	add_child(my_own_little_pingus)
-	
-	var ipg := IPGopher.new()
-	ipg.ip_fetching_finished.connect(
-		func(result: IPGopher.IpFetchingErrs):
-			match result:
-				IPGopher.IpFetchingErrs.OK:
-					menu._set_ip_label(ipg.get_address_as_string())
-				IPGopher.IpFetchingErrs.BAD_RESULT:
-					printerr("BAD_RESULT. Retrying... ")
-					ipg._attempt_addr_fetch()
-				IPGopher.IpFetchingErrs.BAD_RESPONSE:
-					printerr("BAD_RESPONSE. Retrying... ")
-					ipg._attempt_addr_fetch()
-				IPGopher.IpFetchingErrs.BAD_IP:
-					printerr("BAD_IP. Retrying... ")
-					ipg._attempt_addr_fetch()
-	)
-	ipg.set_name("IPGopher")
-	add_child(ipg)
+   print(IP.get_local_addresses())
+   var arguments = {}
+   for argument in OS.get_cmdline_args():
+      # Parse valid command-line arguments into a dictionary
+      if argument.find("=") > -1:
+         var key_value = argument.split("=")
+         arguments[key_value[0].lstrip("--")] = key_value[1]
+   if arguments.keys().has("port"):
+      print(arguments["port"])
+      my_own_little_pingus = PingusPrime.new(int(arguments["port"]))
+   
+   menu.connect_button_pressed.connect(touch_something_with_my_own_little_pingus)
+   menu.send_msg_button_pressed.connect(touch_something_with_my_own_little_stringus)
+   
+   var handle_IP_singleton: Callable 
+   handle_IP_singleton = func(msg: String, type: PingusPrime.SignalTypes) -> void:
+      if msg != "IP RECIEVED" or type != PingusPrime.SignalTypes.CONTROL: printerr("INVALID MESSAGE ARRIVED EARLY")
+      for function in my_own_little_pingus.message_recieved.get_connections():
+         my_own_little_pingus.message_recieved.disconnect(function["callable"])
+      my_own_little_pingus.message_recieved.connect(_recieve_message)
+      menu._set_ip_label(my_own_little_pingus.ExternAddr)
+   my_own_little_pingus.message_recieved.connect(handle_IP_singleton)
+   
+   my_own_little_pingus.set_name("my_own_little_pingus")
+   add_child(my_own_little_pingus)
 
 func touch_something_with_my_own_little_pingus(Addr: String) -> void:
-	if my_own_little_pingus:
-		my_own_little_pingus.TargetAddr = Addr
+   if my_own_little_pingus:
+      my_own_little_pingus.TargetAddr = Addr
 func touch_something_with_my_own_little_stringus(Msg: String) -> void:
-	if my_own_little_pingus:
-		my_own_little_pingus.send_stringus(Msg)
+   if my_own_little_pingus:
+      my_own_little_pingus.send_stringus(Msg)
 
 func _recieve_message(Msg: String, Type: PingusPrime.SignalTypes) -> void:
-	match Type:
-		PingusPrime.SignalTypes.DATA:
-			menu._append_to_chatlog(Msg)
-		PingusPrime.SignalTypes.CONTROL:
-			menu.MESSAGE_LABEL.text = "[THEM]: " + Msg
-	if my_own_little_pingus.PingusState == PingusPrime.PingusStates.CONNECTED:
-		menu._toggle_fields(true)
+   match Type:
+      PingusPrime.SignalTypes.DATA:
+         menu._append_to_chatlog("[THEM]: " + Msg)
+      PingusPrime.SignalTypes.CONTROL:
+         menu.MESSAGE_LABEL.text = Msg
+   if my_own_little_pingus.PingusState == PingusPrime.PingusStates.CONNECTED:
+      menu._toggle_fields(true)
--- menu.gd
+++ menu.gd
@@ -1,7 +1,8 @@
 extends Control
 class_name MenuNode
 
-@onready var IP_LABEL       : Label         = $HBoxContainer/VBoxContainer/Label
+@onready var IP_LABEL       : Label         = $HBoxContainer/VBoxContainer/YourIP/Label
+@onready var COPY_IP_BUTTON : Button        = $HBoxContainer/VBoxContainer/YourIP/Button
 @onready var IP_FIELDS      : HBoxContainer = $HBoxContainer/VBoxContainer/IPfields
 @onready var IP_ENTRY_BOX   : TextEdit      = $HBoxContainer/VBoxContainer/IPfields/TextEdit
 @onready var CONNECT_BUTTON : Button        = $HBoxContainer/VBoxContainer/IPfields/Button
@@ -16,39 +17,51 @@ signal send_msg_button_pressed(Message: String)
 # ultility #
 # ======== #
 func _ready() -> void:
-	CONNECT_BUTTON.pressed.connect(_on_connect_button_pressed)
-	SEND_MSG_BUTTON.pressed.connect(_on_send_msg_button_pressed)
+   CONNECT_BUTTON.pressed.connect(_on_connect_button_pressed)
+   SEND_MSG_BUTTON.pressed.connect(_on_send_msg_button_pressed)
+   _toggle_fields(false)
+func _process(_delta: float) -> void:
+   if Input.is_action_just_pressed("enter"):
+      if IP_FIELDS.visible:
+         CONNECT_BUTTON.pressed.emit()
+         IP_ENTRY_BOX.grab_focus()
+      elif MSG_FIELDS.visible:
+         SEND_MSG_BUTTON.pressed.emit()
+         MSG_ENTRY_BOX.grab_focus()
 func _toggle_fields(msg_visible: bool) -> void:
-	IP_FIELDS.visible = not msg_visible
-	MSG_FIELDS.visible = msg_visible
+   if msg_visible: MSG_ENTRY_BOX.focus_mode = Control.FOCUS_ALL
+   else          : IP_ENTRY_BOX.focus_mode = Control.FOCUS_ALL
+   IP_FIELDS.visible = not msg_visible
+   MSG_FIELDS.visible = msg_visible
 func is_valid_ip_port(input_text: String) -> bool:
-	var regex = RegEx.new()
-	regex.compile("^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$")
-	var result = regex.search(input_text)
-	return true if result else false
+   var regex = RegEx.new()
+   regex.compile("^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$")
+   var result = regex.search(input_text)
+   return true if result else false
 # ========= #
 # ip fields #
 # ========= #
 func _set_ip_label(input_text: String) -> void:
-	if is_valid_ip_port(input_text):
-		IP_LABEL.text = "YOUR IP: " + input_text
-		MESSAGE_LABEL.text = "IP copied to clipboard! :3"
-		DisplayServer.clipboard_set(input_text)
+   if is_valid_ip_port(input_text):
+      IP_LABEL.text = "YOUR IP: " + input_text
+      COPY_IP_BUTTON.pressed.connect(DisplayServer.clipboard_set.bind(input_text))
 func _on_connect_button_pressed() -> void:
-	var input_text = IP_ENTRY_BOX.text.strip_edges()
-	if is_valid_ip_port(input_text):
-		MESSAGE_LABEL.text = "CONNECTING TO: " + input_text
-		connect_button_pressed.emit(input_text)
-	else: 
-		MESSAGE_LABEL.text = input_text + " is not a valid IP address."
+   var input_text = IP_ENTRY_BOX.text.strip_edges()
+   if is_valid_ip_port(input_text):
+      MESSAGE_LABEL.text = "CONNECTING TO: " + input_text
+      connect_button_pressed.emit(input_text)
+   else: 
+      IP_ENTRY_BOX.text = ""
+      MESSAGE_LABEL.text = input_text + " is not a valid IP address."
 # ========== #
 # msg fields #
 # ========== #
 func _on_send_msg_button_pressed() -> void:
-	var input_text = MSG_ENTRY_BOX.text.strip_edges()
-	MSG_ENTRY_BOX.text = ""
-	_append_to_chatlog("[YOU]: " + input_text)
-	send_msg_button_pressed.emit(input_text)
+   var input_text = MSG_ENTRY_BOX.text.strip_edges()
+   MSG_ENTRY_BOX.text = ""
+   if input_text:
+      _append_to_chatlog("[YOU]: " + input_text)
+      send_msg_button_pressed.emit(input_text)
 func _append_to_chatlog(Msg: String) -> void:
-	CHAT_LOG.text += Msg+"\n" 
-	CHAT_LOG.scroll_vertical = CHAT_LOG.get_line_count()
+   CHAT_LOG.text += Time.get_time_string_from_system() + " " + Msg+"\n" 
+   CHAT_LOG.scroll_vertical = CHAT_LOG.get_line_count()
